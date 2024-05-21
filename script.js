99ef833af03aad3a55d7fafe205b984ebcf7db64
Gokay <gokaybalkaya@gmail.com>
2024-05-21 21:00:25 +0200

    Initialize Repository

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-const panels = document.querySelectorAll('.panel')
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
-}
+open.addEventListener('click', () => container.classList.add('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove('show-nav'))
